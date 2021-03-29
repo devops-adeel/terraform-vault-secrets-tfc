b9437e46cf82ed834f677c7149621c642569f8cf
Adeel Ahmad <adeel@hashicorp.com>
2021-03-29 14:23:39 +0100

    fix: amended output to backend

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "backend_path" {
   description = "Secrets Backend Path as output"
-  value       = vault_terraform_cloud_secret_backend.default.path
+  value       = vault_terraform_cloud_secret_backend.default.backend
 }
