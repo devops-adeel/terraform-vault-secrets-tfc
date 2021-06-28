64455fc976e8e628a366a7de12b73a30b868882b
Adeel Ahmad <adeel@hashicorp.com>
2021-06-28 21:49:12 +0100

    feat: added admin policy for github team

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Usage:
 ```hcl
 
 module "vault_tfc_secrets" {
-  source      = "git::https://github.com/devops-adeel/terraform-vault-secrets-tfc.git?ref=v0.2.0"
+  source      = "git::https://github.com/devops-adeel/terraform-vault-secrets-tfc.git?ref=v0.4.0"
 }
 ```
 
@@ -35,8 +35,10 @@ No modules.
 |------|------|
 | [vault_identity_group.default](https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/identity_group) | resource |
 | [vault_identity_group_policies.default](https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/identity_group_policies) | resource |
+| [vault_policy.admin](https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/policy) | resource |
 | [vault_policy.default](https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/policy) | resource |
 | [vault_terraform_cloud_secret_backend.default](https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/terraform_cloud_secret_backend) | resource |
+| [vault_policy_document.admin](https://registry.terraform.io/providers/hashicorp/vault/latest/docs/data-sources/policy_document) | data source |
 | [vault_policy_document.default](https://registry.terraform.io/providers/hashicorp/vault/latest/docs/data-sources/policy_document) | data source |
 
 ## Inputs
@@ -51,4 +53,5 @@ No modules.
 |------|-------------|
 | <a name="output_backend_path"></a> [backend\_path](#output\_backend\_path) | Secrets Backend Path as output |
 | <a name="output_identity_group_id"></a> [identity\_group\_id](#output\_identity\_group\_id) | ID of the created Vault Identity Group. |
+| <a name="output_tfc_admin_policy_name"></a> [tfc\_admin\_policy\_name](#output\_tfc\_admin\_policy\_name) | Name of the terraform admin policy |
 <!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
--- integration_tests/fixtures/main.tf
+++ integration_tests/fixtures/main.tf
@@ -1,45 +1,67 @@
 locals {
-  app_prd = "team-prd"
-  app_dev = "team-dev"
-  env_prd = "prd"
-  env_dev = "dev"
-  service = "web"
+  app_prd       = "team-prd"
+  app_dev       = "team-dev"
+  env_prd       = "prd"
+  env_dev       = "dev"
+  service       = "web"
+  token         = var.token
+  username      = var.username
+  organization  = var.organization
+  team          = "terraform-vault-auth-github"
+  vault_address = data.terraform_remote_state.default.outputs.vault_public_endpoint_url
+  namespace     = "admin/terraform-vault-secrets-tfc"
 }
 
 module "default" {
   source = "./module"
-  token  = var.token
+  token  = local.token
 }
 
 data "vault_auth_backend" "default" {
   path = "approle"
 }
 
+resource "vault_github_auth_backend" "default" {
+  organization = local.team
+}
+
+module "vault_github_team" {
+  source         = "git::https://github.com/devops-adeel/terraform-vault-auth-github.git?ref=v0.1.0"
+  github_team    = local.team
+  env            = local.env_prd
+  service        = local.service
+  mount_accessor = vault_github_auth_backend.default.accessor
+  policies       = [module.default.tfc_admin_policy_name]
+}
+
 ####
 ## Example of prod deployment in same namespace, using the same Vault ACL Policy
 ####
 
-module "prd" {
+module "prd_approle" {
   source            = "git::https://github.com/devops-adeel/terraform-vault-approle.git?ref=v0.7.0"
   application_name  = local.app_prd
   env               = local.env_prd
   service           = local.service
   mount_accessor    = data.vault_auth_backend.default.accessor
-  identity_group_id = module.default.identity_group_id
+  identity_group_id = module.vault_static_secrets.identity_group_id
 }
 
-resource "tfe_team" "prd" {
-  name         = local.app_prd
-  organization = var.organization
+module "vault_static_secrets" {
+  source = "git::https://github.com/devops-adeel/terraform-vault-secrets-kv.git?ref=v0.3.0"
 }
 
-resource "vault_terraform_cloud_secret_role" "prd" {
-  backend      = module.default.backend_path
-  name         = local.app_prd
-  organization = var.organization
-  team_id      = tfe_team.prd.id
+module "prd_workspace" {
+  source               = "git::https://github.com/devops-adeel/terraform-tfc-workspace.git?ref=v0.1.0"
+  application_name     = local.app_prd
+  vault_namespace      = local.namespace
+  username             = local.username
+  vault_address        = local.vault_address
+  organization         = local.organization
+  vault_approle_id     = module.prd_approle.approle_id
+  vault_approle_secret = module.prd_approle.approle_secret
+  backend_path         = module.default.backend_path
 }
-
 
 ####
 ## Example of dev deployment in same namespace, using the same Vault ACL Policy
@@ -56,13 +78,13 @@ module "dev" {
 
 resource "tfe_team" "dev" {
   name         = local.app_dev
-  organization = var.organization
+  organization = local.organization
 }
 
 resource "vault_terraform_cloud_secret_role" "dev" {
   backend      = module.default.backend_path
   name         = local.app_dev
-  organization = var.organization
+  organization = local.organization
   team_id      = tfe_team.dev.id
 }
 
